1a625a41db70e26d0b9d02b55dddbdb5bb80704b
Egor <hoboman313@gmail.com>
2015-11-09 23:03:16 -0800

    more libs to work with, bootstrap starter template stuff

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,8 +2,10 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
+  var app = new EmberApp({
+    emberCliFontAwesome: {
+      useScss: true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
