ddf053c9f7b516792cfc1e354c6437f7e10c583d
Egor <hoboman313@gmail.com>
2015-11-23 22:36:02 -0800

    better sizing for the resume page, getting started on the projecs page

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'egorphilippov',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -19,9 +19,9 @@ module.exports = function(environment) {
     },
 
     contentSecurityPolicy: {
-      'font-src': "'self' fonts.gstatic.com",
-      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com",
-      'script-src': "'self' 'unsafe-inline' www.google-analytics.com",
+      'font-src': "'self' fonts.gstatic.com cdn.jsdelivr.net",
+      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com cdn.jsdelivr.net",
+      'script-src': "'self' 'unsafe-inline' www.google-analytics.com cdn.jsdelivr.net",
       'frame-src': "docs.google.com"
     }
   };
