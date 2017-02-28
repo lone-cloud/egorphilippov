48bce9d0fe16b354262bfd40fea7bd3129c18bc6
Egor <hoboman313@gmail.com>
2017-02-27 18:37:04 -0800

    correctly including ember font awesome assets

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,9 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   var app = new EmberApp({
-    emberCliFontAwesome: {
+    'ember-font-awesome': {
       useScss: true
     }
   });
