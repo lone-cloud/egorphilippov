dc9358311a1b7c5c0979291ff17846804d5d1195
Egor <hoboman313@gmail.com>
2018-03-17 00:05:20 -0700

    no need for contact form component, yarn upgrade

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,9 +1,7 @@
-import Component from '@ember/component';
+import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
-export default Component.extend({
-  tagName: '',
-
+export default Controller.extend({
   submitEnabled: computed('email', 'message', function () {
     return this.get('email.length') >= 3 && this.get('message.length') >= 3;
   })
