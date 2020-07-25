02fcb9c2edc28413970e35ded12f76565dc327b9
Egor <hoboman313@gmail.com>
2020-07-25 01:34:47 -0700

    resume and dependency updates

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,8 +1,8 @@
 import Controller from '@ember/controller';
-import { get, computed } from '@ember/object';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   submitEnabled: computed('email.length', 'message.length', function () {
-    return get(this, 'email.length') >= 3 && get(this, 'message.length') >= 3;
+    return this.email && this.email.length >= 3 && this.message && this.message.length >= 3;
   }),
 });
