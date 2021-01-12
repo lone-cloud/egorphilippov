bddd2414a72b282fa404f6baea5288bcd1bc352d
Egor <egor@nidratech.com>
2021-01-11 19:28:55 -0800

    upgrade deps, fix fontawesome icon flashing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { darken } from 'polished';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 import { GlobalStyles, theme } from 'styles';
 import { Navbar } from 'components';
 
