3b2eaab1b3c05d0dcb6c35e4cfa2e2647bcb7c56
Egor <egor@nidratech.com>
2021-03-09 21:35:09 -0800

    blog post #3

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -15,12 +15,54 @@ const Clients: NextPage = () => {
         <BlogIndex>
           <BlogIndexYear>
             <YearTitle>2021</YearTitle>
-            <BlogEntryLink href="#huegasm-is-free">
-              Huegasm is now free and a look into the future...
-            </BlogEntryLink>
-            <BlogEntryLink href="#good-night-sweet-tomati">Good night, sweet Tomati</BlogEntryLink>
+            <div>
+              <BlogEntryLink href="#the-rise-of-the-long-tailed-beast">
+                The rise of the long tailed beast
+              </BlogEntryLink>
+            </div>
+
+            <div>
+              <BlogEntryLink href="#huegasm-is-free">
+                Huegasm is now free and a look into the future...
+              </BlogEntryLink>
+            </div>
+
+            <div>
+              <BlogEntryLink href="#good-night-sweet-tomati">
+                Good night, sweet Tomati
+              </BlogEntryLink>
+            </div>
           </BlogIndexYear>
         </BlogIndex>
+
+        <BlogEntry id="the-rise-of-the-long-tailed-beast">
+          <EntryTitle>The rise of the long tailed beast</EntryTitle>
+          <EntryDate>Mar. 9, 2021</EntryDate>
+          <EntryBody>
+            Some time ago I watched a{' '}
+            <a href="https://www.youtube.com/watch?v=zb_svgYYh9U" target="_blank" rel="noreferrer">
+              pretty cool video
+            </a>{' '}
+            about &quot;long tail websites&quot;. A long tail website aims to attract visitors
+            through its sheer number of pages and visitors can be monetized through ads. Of course,
+            and as always, marketing and SEO are key. An example of a successful long tail site
+            could be a thesaurus website listing hundreds of thousands of pages for different words.
+            Much of the site content could consist of data scrapped (ex. sample sentences from
+            children&apos;s books), which is exactly what the video creator claims to have done. In
+            essence, this is a way to monetize data. The main challenge here is figuring out how to
+            collect, analyze and present big data in an interesting way.
+            <Spacer />
+            Having just moved on from Tomati that had hundreds of thousands of restaurant inspection
+            data I got this idea 🤔... and that&apos;s how{' '}
+            <a href="https://score.restaurant/" target="_blank" rel="noreferrer">
+              https://score.restaurant/
+            </a>{' '}
+            was born. Will it succeed? Will it get visitors? I don&apos;t know, but I want to give
+            it one more shot. At the very least it was a worthwhile experience to get a blueprint to
+            quickly (and freely) create and deploy sophisticated web applications. In my next blog
+            post I&apos;ll detail exactly how I did it.
+          </EntryBody>
+        </BlogEntry>
 
         <BlogEntry id="huegasm-is-free">
           <EntryTitle>Huegasm is now free and a look into the future...</EntryTitle>
@@ -28,14 +70,15 @@ const Clients: NextPage = () => {
           <EntryBody>
             About 5 years ago I moved to a new apartment and wanted to synchronize my music to the
             smart light bulbs in my living room.{' '}
-            <a target="blank" href="http://www.huegasm.com/">
+            <a href="http://www.huegasm.com/" target="_blank" rel="noreferrer">
               And so Huegasm was born.
             </a>{' '}
             Originally developed solely as a web app for synchronizing music to Philips Hue lights,
             its best iteration came in the form of a{' '}
             <a
-              target="blank"
               href="https://chrome.google.com/webstore/detail/huegasm-for-philips-hue-l/mbjanbdhcpohhfecjgbdpcfhnnbofooj"
+              target="_blank"
+              rel="noreferrer"
             >
               Chrome browser extension
             </a>{' '}
@@ -43,8 +86,9 @@ const Clients: NextPage = () => {
             <Spacer />
             Why the sudden generosity? The Chrome browser{' '}
             <a
-              target="blank"
               href="https://developer.chrome.com/docs/webstore/cws-payments-deprecation/"
+              target="_blank"
+              rel="noreferrer"
             >
               team announced that they&apos;ll be removing the billing API
             </a>
@@ -110,7 +154,7 @@ const Clients: NextPage = () => {
 };
 
 const BlogEntryLink = styled.a`
-  display: block;
+  display: inline-block;
   margin-bottom: ${({ theme }) => theme.spacing.small};
 `;
 const BlogIndex = styled.div`
