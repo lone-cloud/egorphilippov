aea3bc92b6f722c4249c89945e2457eade87fa51
Egor <egor@philippov.ca>
2025-07-24 15:44:54 -0700

    Updates

---
+++ _next/static/chunks/9c4e2130-4c20535a1f9739b6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[512],{1013:(t,e,n)=>{n.d(e,{fad:()=>a});var r=n(4436);function a(t){return(0,r.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"48",d:"m112 244 144-144 144 144M256 120v292"},child:[]}]})(t)}}}]);
