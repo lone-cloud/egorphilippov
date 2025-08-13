301702ee5e07a4b0fbe268979729b6270c53ec57
Egor <egor@philippov.ca>
2025-08-13 12:58:49 -0700

    Updates

---
+++ _next/static/chunks/app/blog/[slug]/page-5097581c647defd3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[953],{2662:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,5765,23)),Promise.resolve().then(n.t.bind(n,1002,23))}},e=>{e.O(0,[2,765,571,266,358],()=>e(e.s=2662)),_N_E=e.O()}]);
